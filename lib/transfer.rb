35b1c1da08295ba2b11d5e320b17636d0a285b72
Donovan Jones <donovanljones94@gmail.com>
2019-09-26 01:10:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,13 +15,12 @@ class Transfer
     end
   end
   
-  def execute_transaction
+ def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    end
-    if @sender.balance < @amount
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
